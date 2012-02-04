acc4a264f48e1c8230ad5b28f6147d13eae25645
Przemek <przemek@belial.(none)>
2012-02-04 17:51:56 +0100

    rq/rs handler redesigned

---
+++ src/main/scala/com/github/amon/rpc/Response.scala
@@ -0,0 +1,27 @@
+/*
+* Copyright 2011 P.Budzik
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* User: przemek
+* Date: 1/29/12
+* Time: 7:07 PM
+*/
+
+package com.github.amon.rpc
+
+case class Response(content: Array[Byte])
+
+object Response {
+  def apply(s: String): Response = Response(s.getBytes)
+}
